5a0aef6094fa9f3cf5da8556e681fb6e958744c3
Francisco Souza <f@souza.cc>
2013-07-14 17:51:03 -0300

    enquetes/admin: criando OpcaoInline
    No Django admin, inlines são utilizados para editar em um só formulário
    diversos itens que dependem do item pai. Neste caso, o OpcaoInline será
    utilizado para editar, pelo admin, as opções de uma enquete na página de
    edição da enquete, sendo possível cadastrar a enquete e todas as opções
    em apenas uma página.
    
    Screenshot: http://fsouza.net/screenshot-enquete.

--- tdc/tdc/enquetes/admin.py
+++ tdc/tdc/enquetes/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from tdc.enquetes import models
 
+
+class OpcaoInline(admin.StackedInline):
+    model = models.Opcao
+
 admin.site.register(models.Enquete)
--- tdc/tdc/enquetes/tests/test_admin.py
+++ tdc/tdc/enquetes/tests/test_admin.py
@@ -1,7 +1,7 @@
 from django import test
 from django.contrib import admin as django_admin
 
-from tdc.enquetes import models
+from tdc.enquetes import admin, models
 
 django_admin.autodiscover()
 
@@ -11,3 +11,12 @@ class EnqueteTestCase(test.TestCase):
     def test_deve_estar_registrada(self):
         registry = django_admin.site._registry
         self.assertIn(models.Enquete, registry)
+
+
+class OpcaoInlinceTestCase(test.TestCase):
+
+    def test_deve_ser_stacked(self):
+        assert issubclass(admin.OpcaoInline, django_admin.StackedInline)
+
+    def test_model_deve_ser_Opcao(self):
+        self.assertEqual(models.Opcao, admin.OpcaoInline.model)
